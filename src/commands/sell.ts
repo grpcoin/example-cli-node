7824068b6bd1cb582285ce8a079cade04ad40079
Kaan Karakaya <yusufkaan142@gmail.com>
2021-05-27 12:37:33 +0300

    Sell coin Signed-off-by: Kaan Karakaya <yusufkaan142@gmail.com>

--- src/commands/sell.ts
+++ src/commands/sell.ts
@@ -14,67 +14,91 @@
  * limitations under the License.
  */
 
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
 
-import * as grpc from '@grpc/grpc-js'
-import * as protoLoader from '@grpc/proto-loader'
-import {PaperTradeClient} from '../proto/grpcoin_grpc_pb'
-import * as path from 'path'
-import {Amount, TradeAction, TradeRequest, TradeResponse} from '../proto/grpcoin_pb'
+import * as grpc from "grpc";
+import { AccountClient, PaperTradeClient } from "../proto/grpcoin_grpc_pb";
+import {
+  QuoteTicker,
+  TestAuthRequest,
+  TestAuthResponse,
+  TradeAction,
+  TradeRequest,
+  TradeResponse,
+} from "../proto/grpcoin_pb";
 
+import { Parse } from "../lib/utils";
 export default class Sell extends Command {
-  static description = 'Sell coin'
+  static description = "Sell coin";
 
-  static examples = [
-    `$ grpcoin sell 2.5
-`,
-  ]
+  static examples = [`$ grpcoin sell 2.5`];
 
   static flags = {
-    help: flags.help({char: 'h'}),
-  }
+    help: flags.help({ char: "h" }),
+  };
 
-  static args = [{name: 'amount'}]
+  static args = [{ name: "amount" }];
 
   async run() {
-    const {args} = this.parse(Sell)
-    const token = process.env.TOKEN
-    const server = 'grpcoin-main-kafjc7sboa-wl.a.run.app:443'
-    const options = {
-      keepCase: true,
-      longs: String,
-      enums: String,
-      defaults: true,
-      oneofs: true,
-    }
+    const { args } = this.parse(Sell);
+    const token = process.env.TOKEN;
+    const server = "grpcoin-main-kafjc7sboa-wl.a.run.app:443";
 
-    // eslint-disable-next-line no-path-concat
-    const PROTO_PATH = path.join(__dirname + '../../../proto/grpcoin.proto')
     if (!token) {
-      this.error('Create a permissionless Personal Access Token on GitHub https://github.com/settings/tokens and set it to TOKEN environment variable')
+      this.error(
+        "Create a permissionless Personal Access Token on GitHub https://github.com/settings/tokens and set it to TOKEN environment variable"
+      );
     }
 
     if (!Number(args.amount)) {
-      this.error('Amount must be number')
+      this.error("Amount must be number");
     }
-    const definition = await protoLoader.load(PROTO_PATH, options)
-    const {grpcoin}: any = grpc.loadPackageDefinition(definition)
 
-    const client: PaperTradeClient = new grpcoin.Account(server, grpc.credentials.createInsecure())
+    const accountClient: AccountClient = new AccountClient(
+      server,
+      grpc.credentials.createSsl()
+    );
 
-    const meta = new grpc.Metadata()
-    meta.add('authorization', 'Bearer ' + token)
+    const meta = new grpc.Metadata();
+    meta.add("authorization", "Bearer " + token);
 
-    // eslint-disable-next-line new-cap
-    const am = new Amount()
-    am.setNanos(5000)
+    await accountClient.testAuth(
+      new TestAuthRequest(),
+      meta,
+      (err: Error | null, response: TestAuthResponse) => {
+        if (err) {
+          this.error(err);
+        }
 
-    const sa = new TradeRequest()
-    sa.setAction(TradeAction.SELL)
-    sa.setQuantity(am)
+        this.log("Login:", response.getUserId());
+      }
+    );
 
-    client.trade(sa, meta, (err: Error | null, response: TradeResponse) => {
-      this.log('Greeting:', response)
-    })
+    const tradeClient: PaperTradeClient = new PaperTradeClient(
+      server,
+      grpc.credentials.createSsl()
+    );
+
+    const trade = new TradeRequest();
+    trade.setAction(TradeAction.SELL);
+    trade.setQuantity(Parse(args.amount));
+    trade.setTicker(new QuoteTicker().setTicker("BTC"));
+
+    // Execute trade
+    tradeClient.trade(
+      trade,
+      meta,
+      (err: Error | null, response: TradeResponse) => {
+        if (err) {
+          this.error(err);
+        }
+        this.log(
+          "ORDER EXECUTED: %s [%s] coins at USD[%s]",
+          response.getAction(),
+          response.getQuantity(),
+          response.getExecutedPrice()
+        );
+      }
+    );
   }
 }
